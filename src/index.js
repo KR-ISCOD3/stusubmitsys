e424c84669cc3c6362bb135ffd98e094824f93ed
KR-ISCOD3 <krisjenxz@example.com>
2024-12-10 13:13:38 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middleware = jsonServer.defaults();
+
+const {config} = require('dotenv')
+config();
+
+server.use(middleware);
+server.use(router);
+
+const PORT = process.env.PORT || 3006;
+
+server.listen(PORT,()=>{
+    console.log(`http://localhost:${PORT}`);
+})
